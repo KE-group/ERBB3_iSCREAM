e336a31ee4fcddc07fa3af4e09db7baff36562d3
dchakro <35454738+dchakro@users.noreply.github.com>
2022-11-10 22:55:49 +0200

    combine data from

--- bootstrap/2. combine and FC calc.R
+++ bootstrap/2. combine and FC calc.R
@@ -1,48 +1,61 @@
-
-
-
+## Description: Combining readcounts for genotypes and calculate FC
+# ---- Thu, Nov 10, 2022 @ 22:19 ----
+rm(list = ls())
+gc()
+library(data.table)
 #--------> Combining files <------------
 rm(list=ls())
+# True Hits
 setwd("/Volumes/DATA/Seafile/NGS-Data-DC/PacBio/20210909 iSCREAM Run1/analysis/Figures/Bootstrap/Data/")
 dir.create("./combined_hits", showWarnings = F)
 
-data.files <- list.files(".",pattern = "V1")
-
-dat <- data.table()
-for(FILE in data.files){
-  dat <- rbind(dat,fread(FILE))
+combine <- function(dirname){
+  setwd(dirname)
+  data.files <- list.files(".",pattern = "V1")
+  
+  dat <- data.table()
+  for(FILE in data.files){
+    dat <- rbind(dat,fread(FILE))
+  }
+  
+  genotypes <- unique(dat$muts)
+  dat <- data.table(genotypes,
+                    WEHI = NA,
+                    NRG = NA,
+                    NRG_to_NoLig = NA)
+  
+  # NRG_to_NoLig
+  tmp <-
+    fread(data.files[grep(pattern = "NRG_to_NoLig", 
+                          x = data.files)])
+  idx <- match(x = dat$genotypes,table = tmp$muts)
+  dat$NRG_to_NoLig <- tmp$N[idx]
+  
+  tmp <- fread(data.files[grep(pattern = "NRG_[^to]", 
+                               x = data.files)])
+  idx <- match(x = dat$genotypes,table = tmp$muts)
+  dat$NRG <- tmp$N[idx]
+  
+  tmp <- fread(data.files[grep(pattern = "WEHI", 
+                               x = data.files)])
+  idx <- match(x = dat$genotypes,table = tmp$muts)
+  dat$WEHI <- tmp$N[idx]
+  rm(tmp)
+  setorder(dat, -NRG_to_NoLig,na.last = T)
+  
+  
+  dat[,NRG_to_NoLig := NRG_to_NoLig / 291190] # total reads (from flagstat)
+  dat[,NRG := NRG / 217694] # total reads (from flagstat)
+  dat[,WEHI := WEHI / 144629]   # total reads (from flagstat)
+  
+  dat[, FC := NRG_to_NoLig/NRG]
+  dat <- dat[!is.na(FC),]
+  dat[, FC_text := format(FC, scientific = F,digits = 2)]
+  setorder(dat, -FC, na.last = T)
+  setwd("..")
+  return(dat)
 }
 
-genotypes <- unique(dat$muts)
-dat <- data.table(genotypes,
-                  WEHI = NA,
-                  NRG = NA,
-                  NRG_to_NoLig = NA)
+hits <- combine(dirname = "20221110_214857/")
+set1 <- combine(dirname = "20221110_220920/")
 
-# Pooling NRG_to_NoLig data
-for(FILE in data.files){
-  print(FILE)
-}
-
-tmp <- fread(data.files[1])
-idx <- match(x = dat$genotypes,table = tmp$muts)
-dat$NRG_to_NoLig <- tmp$N[idx]
-
-tmp <- fread(data.files[2])
-idx <- match(x = dat$genotypes,table = tmp$muts)
-dat$NRG <- tmp$N[idx]
-
-tmp <- fread(data.files[3])
-idx <- match(x = dat$genotypes,table = tmp$muts)
-dat$WEHI <- tmp$N[idx]
-rm(tmp)
-setorder(dat, -NRG_to_NoLig,na.last = T)
-
-write.table(
-  x = dat,
-  file = "combined_hits/read_counts_ERBB3_V1_NRG_to_NoLig.tsv",
-  quote = F,
-  row.names = F,
-  col.names = T,
-  sep = "\t"
-)
